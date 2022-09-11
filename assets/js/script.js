55817e720173b11d39373152469368333bfbe759
bobbeekethdy1025 <bobbeekethdy@gmail.com>
2022-09-10 20:50:28 -0500

    added contents to all files

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var timeToday = moment().format('dddd MMMM Do');
+$('#currentDay').text(timeToday)
+
+
+
+var saveButton = document.querySelector('tbody');
+var task = document.querySelectorAll('textarea').value;
+
+// saveButton.addEventListener('click', function(event){
+//     if (event.target.matches('.saveBtn'))
+//         var timeSlot = event.target.dataset.time;
+//         var dataInSlot = event.target.parentElement.children[1].children.[0].vaule;
+//         localStorage.setItem('timeSlot-'+timeSlot, dataInSlot)
+// })
+
+
